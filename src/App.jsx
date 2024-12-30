0eff7d169a16ef008a485b2afdad3e2a8b8cf5ca
leonardtinozheve <leonardt.zheve@gmail.com>
2024-12-30 05:11:10 -0500

    added the pages to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import Header from "./components/Header.jsx"
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Blog from "./pages/Blog.jsx";
 
 function App() {
   return(<>
