6da9173eae09d044a10ffff0d5cd9f7786d5cbad
leonardtinozheve <leonardt.zheve@gmail.com>
2025-01-11 23:29:54 -0500

    Changed the location of pictures

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,19 +16,19 @@ function Footer() {
 
             <ul className={styles.links}>
                 <li className={styles.link}>
-                    <img src="/assets/contacts/gmail.png" 
+                    <img src="/contacts/gmail.png" 
                     alt="Gmail icon"/>
                     <a href="mailto:leonardt.zheve@gmail.com">leonardt.zheve@gmail.com</a>
                 </li>
 
                 <li className={styles.link}>
-                    <img src="/assets/contacts/linkedin.png" 
+                    <img src="/contacts/linkedin.png" 
                     alt="LinkedIn icon"/>
                     <a href="https://www.linkedin.com/in/leonard-zheve/">My LinkedIn</a>
                 </li>
 
                 <li className={styles.link}>
-                    <img src="/assets/contacts/github.png" 
+                    <img src="/contacts/github.png" 
                     alt="GitHub icon"/>
                     <a href="https://github.com/leonardtinozheve">My GitHub profile</a>
                 </li>
